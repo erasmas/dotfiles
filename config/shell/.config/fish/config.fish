606bf9059a42e091a9149417cb68e4e571756575
Dmytro Morozov <mail@dmorozov.info>
2021-07-05 13:05:29 +0300

    Disable fish default mode prompt

--- config/shell/.config/fish/config.fish
+++ config/shell/.config/fish/config.fish
@@ -32,10 +32,9 @@ set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
 
-# TODO uncomments when fish 3.2.0 is released https://github.com/fish-shell/fish-shell/issues/7458
-# function fish_default_mode_prompt
-#     # NOOP to disable mode indicator
-# end
+function fish_default_mode_prompt
+    # NOOP to disable mode indicator
+end
 
 ## fzf
 
